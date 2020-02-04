85a57682f85c2553c20e1f4cc59c60e29e2e9a2d
Max Martyn <martynmaksim.w@gmail.com>
2020-02-04 22:15:24 +0800

    Learn objects, arrays, tuples, enums

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    return result;
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
+// } = {
+//     name: 'Max',
+//     age: 24,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person = {
+    name: 'Max',
+    age: 24,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+var favouriteActivities;
+favouriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+if (person.role === Role.ADMIN) {
+    console.log('admin');
+}
--- app.ts
+++ app.ts
@@ -1,16 +1,33 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    const result = n1 + n2
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
+// } = {
+//     name: 'Max',
+//     age: 24,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// }
 
-    if (showResult) {
-        console.log(phrase + result)
-    } 
-    
-    return result;
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
+const person = {
+    name: 'Max',
+    age: 24,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
 }
 
-let number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: '
+let favouriteActivities: string[];
+favouriteActivities = ['Sports'];
 
-add(number1, number2, printResult, resultPhrase)
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+    console.log('admin');
+}
