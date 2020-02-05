da13e0ab2360e3184977156892929bdf38ed7228
Max Martyn <martynmaksim.w@gmail.com>
2020-02-05 17:11:40 +0800

    Learn custom types

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
+type Combinable = number | string;
+type Conversion = 'as-number' | 'as-text';
+
 function combine(
-    input1: number | string, 
-    input2: number | string,
-    resultConversion: 'as-number' | 'as-text'    
+    input1: Combinable, 
+    input2: Combinable,
+    resultConversion: Conversion
 ) {
     let result;
     
