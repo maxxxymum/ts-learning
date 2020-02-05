4fc74d8340bf46171a631940a5548bba712af42a
Max Martyn <martynmaksim.w@gmail.com>
2020-02-05 18:39:47 +0800

    Add tsconnfig

--- app.ts
+++ app.ts
@@ -1,15 +1,3 @@
-let userInput: unknown;
-let userName: string;
+const userName = 'Maxxx';
 
-userInput = 5;
-userInput = 'Max';
-
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-
-function generateError(message: string, code: number) : never {
-    throw {message: message, errorCode: code};
-}
-
-generateError('Error', 500);
+console.log(userName);
