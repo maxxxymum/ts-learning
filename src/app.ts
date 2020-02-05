8d0e2a20b0fdfc3e221f58b406a6c553f8a7d26b
Max Martyn <martynmaksim.w@gmail.com>
2020-02-05 19:38:42 +0800

    Learn debugging in VSCode

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 const button = document.querySelector('button');
+let appId = '1';
+
+function add(n1: number, n2: number) {
+    if (n1 + n2 > 0) {
+        return n1 + n2;
+    }
+
+    return;
+}
+
+function clickHandler(message: string) {
+    // let userName = 'Max';
+    console.log('Clicked!' + message);
+}
 
 if (button) {
-    button.addEventListener('click', () => {
-        console.log('Clicked!');
-    });
+    button.addEventListener('click', clickHandler.bind(null, 'YO'));
 }
