1aeda45363e7eef2401058ed8d6f3f9a48f1cb19
Max Martyn <martynmaksim.w@gmail.com>
2020-02-08 12:54:49 +0800

    Learn generics

--- src/app.ts
+++ src/app.ts
@@ -74,3 +74,27 @@ const numberStorage = new DataStorage<number>();
 // objectStorage.addItem({name: 'Bou'});
 // objectStorage.removeItem(max);
 // console.log(objectStorage.getItems());
+
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+
+function createCourseGoal(
+    title: string, 
+    description: string, 
+    date: Date
+): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {};
+
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+
+    return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Max', 'Dima'];
+// names.push('Wayan');
+// names.pop();
