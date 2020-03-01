b0cea293484a258a1825289e17588f449c9f6883
Max Martyn <martynmaksim.w@gmail.com>
2020-03-01 16:46:53 +0200

    Added axios and fixed tsconfig

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,31 @@
+import axios from 'axios';
+
 const form = document.querySelector('form')! as HTMLFormElement;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_MAPS_KEY = 'AIzaSyCak3AYFSUdv0aurGmCuHDMY1EXMMYjr1Q';
+
+type GoogleGeocodingRespone = {
+    results: {geometry: {location: {lat: number, lng: number}}}[];
+    status: 'OK' | 'ZERO_RESULTS';
+};
 
 function searchAddressHandler(event: Event) {
     event.preventDefault();
 
     const enteredAddress = addressInput.value;
+
+    axios.get<GoogleGeocodingRespone>(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_MAPS_KEY}`
+    ).then(response => {
+        if (response.data.status !== 'OK') {
+            throw new Error('Couldn\'t fetch location!');
+        }
+
+        const coordinates = response.data.results[0].geometry.location;
+    }).catch(error => {
+        console.error(error);
+    });
 }
 
 form.addEventListener('submit', searchAddressHandler);
